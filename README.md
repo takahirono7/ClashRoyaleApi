0f5f51f5ab05b2bd8646946ccf5303fbc5f58354
r_shimamura_takahiro <takahiro_shimamura@r.recruit.co.jp>
2018-10-26 23:12:32 +0900

    pro playerの戦闘履歴を取得できるよう修正

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ClashRoyaleApi
 ClashRoyaleのAPIを叩いて様々な情報を取得するpythonスクリプト
+
+# usage
+https://qiita.com/takahirono7/items/4d76d727945b0edd0657
+
+pip install
+    * pyyaml
+    * datetime
+    * json
+    * requests
+    
--- main.py
+++ main.py
@@ -3,23 +3,56 @@ import sys
 import settings
 import requests
 import json
+import datetime
+import os
+import yaml
 
+# 変数定義
 URL = "https://api.clashroyale.com/v1"
+ACCESS_KEY = settings.ACCESS_KEY
+TARGET_API = URL+"/players/"
+HEADERS = {
+    "content-type": "application/json; charset=utf-8",
+    "cache-control": "max-age=60",
+    "authorization": "Bearer  %s" % ACCESS_KEY}
+
+# 現在時刻
+now = datetime.datetime.now()
+DATE = '{0:%Y_%m_%d_%H00}'.format(now)
+
+# ファイルパス関連
+SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+PLAYER_BATTLE_LOG_DIR = os.path.join(SCRIPT_DIR, "pro_player_battle_log", )
+PRO_PLAYER_LIST_FILE = os.path.join(SCRIPT_DIR, "pro_player_list.yml")
+with open(PRO_PLAYER_LIST_FILE) as f:
+    PRO_PLAYERS = yaml.load(f)
+print(PRO_PLAYERS)
 
 
 def main():
-    access_key = settings.ACCESS_KEY
-    target_api = URL+"/players/"
-    playerTag = "%2328UGR809L"
-    # url = target_api+playerTag+"/upcomingchests"
-    url = target_api+playerTag+"/battlelog"
-    headers = {
-        "content-type": "application/json; charset=utf-8",
-        "cache-control": "max-age=60",
-        "authorization": "Bearer  %s" % access_key}
-    r = requests.get(url, headers=headers)
-    data = r.json()
-    print(json.dumps(data, indent=4))
+    for player_tag in PRO_PLAYERS["PRO_PLAYER_LIST"]:
+        # player_tag = "#8QRCJQ9Y"
+        # player_tag = "%2328UGR809L" #takano7
+        print(player_tag)
+        # 宝箱
+        # url = TARGET_API+player_tag+"/upcomingchests"
+        url = TARGET_API+player_tag+"/battlelog"
+
+    # APIを叩いてプレイヤー情報を取得
+        r = requests.get(url, headers=HEADERS)
+        data = r.json()
+        print(json.dumps(data, indent=4))
+
+    # JSON FILE作成
+        write_file = os.path.join(PLAYER_BATTLE_LOG_DIR, player_tag + "_" + DATE)
+        with open(write_file, 'w') as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
 
 if __name__ == '__main__':
     sys.exit(main())
+
+# import yaml
+
+# with open('input.yaml') as file:
+#     obj = yaml.load(file)
+#     print(obj['z'])
